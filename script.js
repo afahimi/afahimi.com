6eb471054efdb069ee7833b998a44d909112dd4f
Amin Fahiminia <afahimi@student.ubc.ca>
2022-12-23 01:09:57 -0800

    initial site

---
+++ script.js
@@ -0,0 +1,23 @@
+const headsButton = document.getElementById("heads-button");
+const tailsButton = document.getElementById("tails-button");
+const resultElement = document.getElementById("result");
+
+headsButton.addEventListener("click", function() {
+  // Coin flip animation goes here
+  let flipResult = Math.random() < 0.5 ? "Heads" : "Tails";
+  if (flipResult === "Heads") {
+    resultElement.innerHTML = "You won!";
+  } else {
+    resultElement.innerHTML = "You lost. Try again?";
+  }
+});
+
+tailsButton.addEventListener("click", function() {
+  // Coin flip animation goes here
+  let flipResult = Math.random() < 0.5 ? "Heads" : "Tails";
+  if (flipResult === "Tails") {
+    resultElement.innerHTML = "You won!";
+  } else {
+    resultElement.innerHTML = "You lost. Try again?";
+  }
+});
